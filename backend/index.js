1c04b27363378b0a5f335c367e98f111b56fc194
neoyongtai <n3oy0ngta1@gmail.com>
2023-12-06 16:08:27 +0800

    added pagination limit constant and build command

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,6 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is Running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -37,6 +33,17 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
